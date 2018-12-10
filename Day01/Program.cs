e8b82b83b449d27ecd9b3002a9b4718e7a0e8109
Bundschuh Martin <martin.bundschuh@ti8m.ch>
2018-12-10 13:33:20 +0100

    Solution for Day01 added

---
+++ Day01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Input.txt"));
+            var frequency = 0L;
+            foreach(var input in inputs)
+            {
+                long.TryParse(input, out var frequencyDrift);
+                frequency += frequencyDrift;
+            }
+            Console.WriteLine(frequency);
+            Console.ReadLine();
+        }
+    }
+}
