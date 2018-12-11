2d1655f16f11c58fa89dd3ef953600c24e924d0a
Bundschuh Martin <martin.bundschuh@ti8m.ch>
2018-12-11 16:32:14 +0100

    Added day4 part 2

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -11,6 +11,7 @@ namespace Day04
     class Program
     {
         const string GuardEventTag = "Guard #";
+        static int MaxMinuteGuardId = 0;
 
         static void Main(string[] args)
         {
@@ -24,17 +25,20 @@ namespace Day04
                     };
                 })
                 .OrderBy(i => i.Time);
-            Part1_PrintMostAsleepGuardId(inputs);
-            //Part2_Print(inputs, points);
+
+            var sleepMinutesByGuard = GetSleepMinuteReport(inputs);
+            Part1_PrintMostAsleepGuard(sleepMinutesByGuard);
+            Part2_PrintMostAsleepMinuteGuard(sleepMinutesByGuard);
             Console.ReadLine();
         }
 
-        private static void Part1_PrintMostAsleepGuardId(IEnumerable<dynamic> inputs)
+        private static Dictionary<int, Dictionary<int, int>> GetSleepMinuteReport(IOrderedEnumerable<dynamic> inputs)
         {
             var sleepMinutesByGuard = new Dictionary<int, Dictionary<int, int>>();
             var guardId = 0;
+            var maxSleepMinutes = 0;
             var feltAsleep = DateTime.MinValue;
-            foreach(var input in inputs)
+            foreach (var input in inputs)
             {
                 string eventTag = input.Event;
                 if (eventTag.Contains(GuardEventTag))
@@ -42,30 +46,48 @@ namespace Day04
                     var splittedEvent = eventTag.Split(GuardEventTag);
                     int.TryParse(splittedEvent[1].Substring(0, splittedEvent[1].IndexOf(" ")), out guardId);
                     sleepMinutesByGuard.TryAdd(guardId, new Dictionary<int, int>());
-                } else if (string.Compare(eventTag, "falls asleep") == 0)
+                }
+                else if (string.Compare(eventTag, "falls asleep") == 0)
                 {
                     feltAsleep = input.Time;
-                } else
+                }
+                else
                 {
                     DateTime wokeUp = input.Time;
                     for (var m = feltAsleep.Minute; m < wokeUp.Minute; m++)
                     {
                         if (!sleepMinutesByGuard[guardId].TryAdd(m, 1))
                         {
-                            sleepMinutesByGuard[guardId][m]++;
+                            var newSleepMinutes = sleepMinutesByGuard[guardId][m]++;
+                            if (newSleepMinutes > maxSleepMinutes) {
+                                maxSleepMinutes = newSleepMinutes;
+                                MaxMinuteGuardId = guardId;
+                            }
                         }
                     }
                 }
             }
-            
+            return sleepMinutesByGuard;
+        }
+
+        private static void Part1_PrintMostAsleepGuard(Dictionary<int, Dictionary<int, int>> sleepMinutesByGuard)
+        {           
             var guardIdMaxAsleep = sleepMinutesByGuard
                 .OrderBy(g => g.Value.Sum(s => s.Value))
                 .LastOrDefault().Key;
-            var mostAsleepMinute = sleepMinutesByGuard[guardIdMaxAsleep]
-                .OrderBy(s => s.Value)
-                .LastOrDefault().Key;
+            var mostAsleepMinute = GetMostAsleepMinuteFromGuard(guardIdMaxAsleep, sleepMinutesByGuard);
 
             Console.WriteLine($"Most asleep GuardId ({guardIdMaxAsleep}) multiplied with most asleep minutes ({mostAsleepMinute}): {guardIdMaxAsleep * mostAsleepMinute}");
         }
+
+        private static void Part2_PrintMostAsleepMinuteGuard(Dictionary<int, Dictionary<int, int>> sleepMinutesByGuard)
+        {
+            var mostAsleepMinute = GetMostAsleepMinuteFromGuard(MaxMinuteGuardId, sleepMinutesByGuard);
+            Console.WriteLine($"Most asleep minutes GuardId ({MaxMinuteGuardId}) multiplied with most asleep minutes ({mostAsleepMinute}): {MaxMinuteGuardId * mostAsleepMinute}");
+        }
+
+        private static int GetMostAsleepMinuteFromGuard(int guardIdMaxAsleep, Dictionary<int, Dictionary<int, int>> sleepMinutesByGuard) => sleepMinutesByGuard[guardIdMaxAsleep]
+            .OrderBy(s => s.Value)
+            .LastOrDefault().Key;
     }
 }
