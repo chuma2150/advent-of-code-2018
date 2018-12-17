45f273b812b6973053f0ba5fdacfffac64fa89d4
Bundschuh Martin <martin.bundschuh@ti8m.ch>
2018-12-17 15:46:06 +0100

    Added day 12 part 2

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -8,7 +8,6 @@ namespace Day12
 {
     class Program
     {
-        const int NUMBER_OF_GENERATIONS = 20;
         const char PLANT_INDICATOR = '#';
         static int offsetX = 3;
 
@@ -24,19 +23,41 @@ namespace Day12
                     return (splittedInput.FirstOrDefault(), char.Parse(splittedInput.LastOrDefault()));
                 });
             Part1_PrintNumberOfPlantsAfter20Generations(initialState, plantPatterns);
+            Part2_PrintNumberOfPlantsAfter5BillionGenerations(initialState, plantPatterns);
             Console.ReadLine();
         }
 
-        private static void Part1_PrintNumberOfPlantsAfter20Generations(string initialState, IEnumerable<(string, char)> plantPatterns)
+        private static void Part1_PrintNumberOfPlantsAfter20Generations(string initialState, IEnumerable<(string, char)> plantPatterns) => CalculateGenerartion(
+            initialState,
+            plantPatterns,
+            20);
+
+        private static void Part2_PrintNumberOfPlantsAfter5BillionGenerations(string initialState, IEnumerable<(string, char)> plantPatterns) => CalculateGenerartion(
+            initialState,
+            plantPatterns,
+            50000000000);
+
+        private static void CalculateGenerartion(string initialState, IEnumerable<(string, char)> plantPatterns, long numberOfGenerations)
         {
             var currentGeneration = initialState;
-            for (var i = 1; i <= NUMBER_OF_GENERATIONS; i++)
+            var previousPlantGrowth = 0L;
+            var previousPlantSum = CalculatePlantSum(currentGeneration);
+            var plantGrowth = 0L;
+            var plantSum = 0L;
+            for (var i = 0; i < numberOfGenerations; i++)
             {
                 currentGeneration = GetNextGeneration(currentGeneration, plantPatterns);
+                plantSum = CalculatePlantSum(currentGeneration);
+                plantGrowth = plantSum - previousPlantSum;
+                if (previousPlantGrowth == plantGrowth)
+                {
+                    plantSum += plantGrowth * (numberOfGenerations - i + 2);
+                    break;
+                }
+                previousPlantGrowth = plantGrowth;
+                previousPlantSum = plantSum;
             }
-
-            var sumOfPlantsPerIndix = CalculatePlantSum(currentGeneration);
-            Console.WriteLine($"Number of plants after {NUMBER_OF_GENERATIONS} Generations: {sumOfPlantsPerIndix}");
+            Console.WriteLine($"Number of plants after {numberOfGenerations} Generations: {plantSum}");
         }
 
         private static string GetNextGeneration(string currentGeneration, IEnumerable<(string pattern, char result)> plantPatterns)
@@ -45,7 +66,7 @@ namespace Day12
             newGeneraton.Append(currentGeneration.Substring(0, 2));
             for (var i = 2; i < currentGeneration.Length - 2; i++)
             {
-                var partToCompare = currentGeneration.Substring(i -2, 5);
+                var partToCompare = currentGeneration.Substring(i - 2, 5);
                 if (plantPatterns.Any(p => p.pattern.Equals(partToCompare)))
                 {
                     var pattern = plantPatterns.Where(p => p.pattern.Equals(partToCompare)).First();
@@ -64,10 +85,10 @@ namespace Day12
             return $"{returnGeneration}...";
         }
 
-        private static int CalculatePlantSum(string generation)
+        private static long CalculatePlantSum(string generation)
         {
-            var sum = 0;
-            for(var i = 0; i < generation.Length; i++)
+            var sum = 0L;
+            for (var i = 0; i < generation.Length; i++)
             {
                 if (generation[i].Equals(PLANT_INDICATOR)) { sum += i - offsetX; }
             }
